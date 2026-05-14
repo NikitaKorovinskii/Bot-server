831a9098ea8a56f349394dbabaf327cdbdeef090
nikitakorovinskij <nikita19999lol@gmail.com>
2026-05-14 12:11:55 +0300

    31fix2: add gym service module

--- app/utils/auth.py
+++ app/utils/auth.py
@@ -4,5 +4,5 @@ ALLOWED_USER_IDS = [
     int(x) for x in os.getenv("ALLOWED_USER_IDS", "").split(",") if x
 ]
 
-def is_allowed(message):
-    return message.from_user.id in ALLOWED_USER_IDS
+def is_allowed_user(user_id: int):
+    return user_id in ALLOWED_USER_IDS
